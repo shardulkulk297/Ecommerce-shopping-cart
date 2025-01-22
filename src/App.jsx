23c54cdd9e3a91c47e8900d9badc9bb2e9bc1752
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2025-01-22 23:38:34 +0530

    Added the the item already in the cart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,37 @@ import { useState } from 'react';
 import './App.css'
 import Navbar from './components/NavBar'
 import Shop from './components/Shop'
+import { Toaster, toast } from 'react-hot-toast';
 function App() {
   
   const [cart, setcart] = useState([]);
+  
 
   const handleClick = (item)=>{
+    // console.log(item)
+    let isPresent = false;
 
-    console.log(item);
+    cart.forEach((product)=>{
+      if(item.id === product.id)
+      {
+        isPresent = true;
+      }
+    })
+
+    if(isPresent){
+      // console.log('Item Already present')
+      toast.error('Item is Already in the Cart')
+      return;
+    }
+
+    setcart([...cart, item] )
     
   }
   
 
   return (
     <>
+    <Toaster position='top-center'></Toaster>
       <Navbar size={cart.length}/>
       <Shop handleClick={handleClick}/>
     </>
