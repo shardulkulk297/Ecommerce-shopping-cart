9fb5b61f16e9db080961c02a575b36cd4e06f4c2
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2025-01-22 23:57:53 +0530

    fixed the cart items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,33 +1,33 @@
 import React, { useState } from 'react'
+import Card from './Card';
+const Cart = ({ cart, setcart }) => {
 
-const Cart = ({cart, setcart}) => {
 
     const [price, setprice] = useState(0);
-  return (
-    <div>
-        {cart?.map((item)=>{
-            return <div className="cart-box" key={item.id}>
-                <div className="cart-img">
-                    <img src={item.img} alt="img" />
-                    <p>{item.title}</p>
+    return (
+        <div>
+            {cart?.map((item) => {
+                return <div className="cart-box" key={item.id}>
+                    <div >
+                        <Card key={item.id}  item={item} />
+
+                    </div>
+                    <div>
+                        <button>+</button>
+                        <button>-</button>
+                    </div>
+                    <div>
+                        <span>{item.price}</span>
+                        <button>Remove</button>
+                    </div>
+
+
 
                 </div>
-                <div>
-                    <button>+</button>
-                    <button>-</button>
-                </div>
-                <div>
-                    <span>{item.price}</span>
-                    <button>Remove</button>
-                </div>
+            })}
 
-
-
-            </div>
-        })}
-      
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Cart
